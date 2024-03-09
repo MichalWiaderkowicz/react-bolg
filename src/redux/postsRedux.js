5be389fc9ad6e64fa4d26bca7516828700584bc1
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-03-09 18:33:24 +0100

    add action POST_REMOVE

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -4,10 +4,13 @@ export const getPost = ({ posts }, postId) =>
   posts.find((post) => post.id === postId);
 // actions
 const createActionName = (actionName) => `app/posts/${actionName}`;
+const POST_REMOVE = createActionName("POST_REMOVE");
 
 // action creators
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case POST_REMOVE:
+      return statePart.filter((post) => post.id !== action.payload);
     default:
       return statePart;
   }
