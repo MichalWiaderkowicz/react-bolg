884cf0114ee6b262e23d124280f2e97d0a5edb46
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-03-13 16:44:28 +0100

    add ADD_POST action wih shortId package

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,3 +1,5 @@
+import shortid from "shortid";
+
 //selectors
 export const getAllPosts = (state) => state.posts;
 export const getPost = ({ posts }, postId) =>
@@ -5,13 +7,17 @@ export const getPost = ({ posts }, postId) =>
 // actions
 const createActionName = (actionName) => `app/posts/${actionName}`;
 const POST_REMOVE = createActionName("POST_REMOVE");
+const ADD_POST = createActionName("ADD_POST");
 
 // action creators
 export const postRemove = (payload) => ({ type: POST_REMOVE, payload });
+export const addPost = (payload) => ({ type: ADD_POST, payload });
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
     case POST_REMOVE:
       return statePart.filter((post) => post.id !== action.payload);
+    case ADD_POST:
+      return [...statePart, { id: shortid(), ...action.payload }];
     default:
       return statePart;
   }
