01d2a4ee7e109b60776e1da452cf3f86e30531dc
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-03-13 16:36:11 +0100

    add onChange to inputs in AddPostForm

--- src/components/Features/AddPostForm/AddPostForm.js
+++ src/components/Features/AddPostForm/AddPostForm.js
@@ -1,37 +1,58 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishedDate, setPublishedDate] = useState("");
+  const [shortDescription, setShortDescription] = useState("");
+  const [content, setContent] = useState("");
   return (
     <Form>
-      <Form.Group className="mb-3">
+      <Form.Group className="m-auto w-75 p-3">
         <Form.Label>Title</Form.Label>
-        <Form.Control placeholder="Enter title" />
+        <Form.Control
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter title"
+        />
       </Form.Group>
-      <Form.Group className="mb-3">
+      <Form.Group className="m-auto w-75 p-3">
         <Form.Label>Author</Form.Label>
-        <Form.Control placeholder="Enter author" />
+        <Form.Control
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Enter author"
+        />
       </Form.Group>
-      <Form.Group className="mb-3">
+      <Form.Group className="m-auto w-75 p-3">
         <Form.Label>Published</Form.Label>
-        <Form.Control placeholder="dd-mm-yyyy" />
+        <Form.Control
+          value={publishedDate}
+          onChange={(e) => setPublishedDate(e.target.value)}
+          placeholder="dd-mm-yyyy"
+        />
       </Form.Group>
-      <Form.Group className="mb-3">
+      <Form.Group className="m-auto w-75 p-3">
         <Form.Label>Short description</Form.Label>
         <Form.Control
+          value={shortDescription}
+          onChange={(e) => setShortDescription(e.target.value)}
           placeholder="Leave a comment here"
           as="textarea"
           rows={3}
         />
       </Form.Group>
-      <Form.Group className="mb-3">
+      <Form.Group className="m-auto w-75 p-3">
         <Form.Label>Main content</Form.Label>
         <Form.Control
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
           placeholder="Leave a comment here"
           as="textarea"
           rows={10}
         />
       </Form.Group>
-
       <Button variant="primary" type="submit">
         Add post
       </Button>
