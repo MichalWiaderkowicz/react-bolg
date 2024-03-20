f28542f2f8a851e62d9f904f8bd10f5e4cedcca6
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2024-03-20 18:21:31 +0100

    task

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/views/Footer/Footer";
 import { Header } from "./components/views/Header/Header";
 import Post from "./components/Features/Post/Post";
-import Home from "./components/pages/home/Home";
-import AddPost from "./components/pages/addPost/AddPost";
-import EditPost from "./components/pages/editPost/EditPost";
-import About from "./components/pages/about/About";
-import NotFound from "./components/pages/notFound/NotFound";
+import Home from "./components/pages/home/home";
+import AddPost from "./components/pages/addPost/addPost";
+import EditPost from "./components/pages/editPost/editPost";
+import About from "./components/pages/about/about";
+import NotFound from "./components/pages/notFound/notFound";
 
 function App() {
   return (
